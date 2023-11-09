d4bc60cee2422690387233c8e76efb0cb2ad5f1a
Ana Rosiles <arosiles@hawk.iit.edu>
2023-11-08 23:38:01 -0600

    trying to troubleshoot menu error

--- js/site.js
+++ js/site.js
@@ -2,5 +2,6 @@ const menuBar = document.getElementById('menuBar');
 const navLinks = document.getElementById('navLinks');
 
 menuBar.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
+  navLinks.classList.toggle('active');
+  console.log('Menu bar clicked'); // Add this line for debugging  
 });
