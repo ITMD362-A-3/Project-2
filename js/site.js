25ce584d8065b71fd60c7f5fa77db3bac39563a8
Ana Rosiles <arosiles@hawk.iit.edu>
2023-11-09 01:20:13 -0600

    got hamburher menu up and running

--- js/site.js
+++ js/site.js
@@ -1,7 +1,9 @@
-const menuBar = document.getElementById('menuBar');
-const navLinks = document.getElementById('navLinks');
 
-menuBar.addEventListener('click', () => {
+
+document.getElementById("menuBar").addEventListener("click", myFunction, false);
+function myFunction() {
+  const navLinks = document.querySelector('.navLinks');
+  console.log('Menu bar clicked');
   navLinks.classList.toggle('active');
-  console.log('Menu bar clicked'); // Add this line for debugging  
-});
+}
+
