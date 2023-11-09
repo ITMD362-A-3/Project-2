b4f4b10c566c092b6fd02e0ec076c362ff427e2c
Ana Rosiles <arosiles@hawk.iit.edu>
2023-11-09 09:15:59 -0600

    adding js for slideshow

--- js/site.js
+++ js/site.js
@@ -1,5 +1,4 @@
-
-
+// menu bar
 document.getElementById("menuBar").addEventListener("click", myFunction, false);
 function myFunction() {
   const navLinks = document.querySelector('.navLinks');
@@ -7,3 +6,33 @@ function myFunction() {
   navLinks.classList.toggle('active');
 }
 
+// wedding slideshow
+// picture slider
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
